f24b69bc8554ab91efe97986016a97c4f64455d3
Nitin Birdi <nitin.birdi@icloud.com>
2020-03-01 14:19:31 +0530

    new commit

--- app.js
+++ app.js
@@ -12,18 +12,23 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 
+app.get("/", function(req, res) {
+  res.render("home", {
+    homeStartingContent: homeStartingContent
+  });
+});
 
-
-
-
-
-
-
-
+app.get("/about", function(req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
+});
 
 
 
