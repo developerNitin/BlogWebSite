bf71d11a46a4c665307c903e05a5e458492b4b08
Nitin Birdi <nitin.birdi@icloud.com>
2020-03-01 21:36:21 +0530

    new things added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -19,10 +20,11 @@ app.use(express.static("public"));
 
 let posts = [];
 
+
 app.get("/", function(req, res) {
   res.render("home", {
     homeStartingContent: homeStartingContent,
-    posts: posts
+    posts: posts,
   });
 });
 
@@ -42,6 +44,20 @@ app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
+app.get("/post/:posts", function(req, res) {
+  let reqs = _.lowerCase(req.params.posts);
+
+  posts.forEach(function(element) {
+     const reqq = _.lowerCase(element.input);
+     if(reqs === reqq) {
+       console.log("match found");
+     } else {
+       console.log("not found");
+     }
+  });
+});
+
+
 app.post("/compose", function(req, res) {
    const post = {
     input: req.body.input,
@@ -50,6 +66,8 @@ app.post("/compose", function(req, res) {
   posts.push(post);
   res.redirect("/");
 });
+
+
 
 
 app.listen(3000, function() {
